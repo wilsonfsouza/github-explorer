305c7dae4194cdcc87f24ab40e527d320430e59a
Wilson Franca <wilson.franca.92@gmail.com>
2020-09-18 19:01:22 -0700

    added the component ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
-const App: React.FC = () => (
-  <>
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
-    <GlobalStyle />
-  </>
-);
+const App: React.FC = () => {
+  const [theme, setTheme] = useState(light);
+  // Could use useCallback
+  const handleToggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  };
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default App;
